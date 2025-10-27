258d93073729e3dfcbac2a7c81fcebfc221f98cd
Seb <s.vandeneede.seb200221@gmail.com>
2025-10-27 17:07:02 +0100

    Started implementing main procedure

--- exact_mif_v3.py
+++ exact_mif_v3.py
@@ -1,3 +1,5 @@
+import itertools
+
 import networkx as nx
 
 
@@ -19,12 +21,77 @@ def get_non_trivial_components(G):
     return components
 
 
+def find_short_pair(G, F, active_v):
+    for u, v in itertools.combinations(set(G.nodes()) - F, 2):
+        nb_u = set(nx.neighbors(G, u))
+        nb_v = set(nx.neighbors(G, v))
+        # If parallel edges between u and v (so u,v is a short-cycle) or they have a common neighbor in F (also short-cycle)
+        if (G.number_of_edges(u, v) > 1) or (
+            G.number_of_edges(u, v) == 1 and any(w in F for w in nb_u & nb_v)
+        ):
+            if (active_v is None) or (active_v not in nb_u & nb_v):
+                return u, v
+
+    return None, None
+
+
 def main_procedure(G, F, active_v):
+    a, b = find_short_pair(G, F, active_v)
+    if a is not None and b is not None:
+        return max(
+            get_mif_len(nx.subgraph(G, set(G.nodes) - {a}), F | {b}, active_v),
+            get_mif_len(nx.subgraph(G, set(G.nodes) - {b}), F, active_v),
+        )
+
+    cut_v = set(nx.articulation_points(G))
+    if len(cut_v) > 0:
+        v = next(iter(cut_v))
+        components = set(nx.connected_components(nx.subgraph(G, set(G.nodes) - {v})))
+        H = min(components, key=len)
+
+        F1 = F & (set(H) | {v})
+        G1 = nx.subgraph(G, set(H) | {v})
+        F2 = F - set(H)
+        G2 = nx.subgraph(G, set(G.nodes) - set(H))
+        F1_star = F1 | {v}
+
+        if v in F:
+            return get_mif_len(
+                G1, F1, active_v if active_v in F1 else None
+            ) + get_mif_len(G2, F2, active_v if active_v in F2 else None)
+
+        sg_H = nx.subgraph(G, H)
+        S1 = get_mif_len(sg_H, F & set(H), active_v if active_v in F & set(H) else None)
+        S1_star = get_mif_len(G1, F1_star, active_v if active_v in F1_star else None)
+
+        if S1_star > S1:
+            return (
+                S1_star - 1 + get_mif_len(G2, F2, active_v if active_v in F2 else None)
+            )
+
+        else:
+            return get_mif_len(
+                sg_H, F1, active_v if active_v in F1 else None
+            ) + get_mif_len(
+                nx.subgraph(G, set(G.nodes) - (set(H) | {v})),
+                F - (set(H) | {v}),
+                active_v if active_v in F - (set(H) | {v}) else None,
+            )
+
+    if len(F) == 0:
+        degrees = dict(nx.degree(G))
+        v = max(degrees, key=degrees.get)
+        new_G = nx.subgraph(G, G.nodes - {v})
+        return max(get_mif_len(G, F | {v}, v), get_mif_len(new_G, F, active_v))
+
+    if active_v is None:
+        active_v = next(iter(F))
+
     pass
 
 
 def get_mif_len(G, F, active_v):
-    if not nx.subgraph(G, F).is_forest():
+    if len(F) > 1 and not nx.subgraph(G, F).is_forest():
         print("Can't reduce cause F is not acyclic")
         return Exception
 
@@ -55,7 +122,7 @@ def get_mif_len(G, F, active_v):
         for n in set(new_G.nodes()) - new_F:
             nb_in_F = set(new_G.neighbors(n)) & new_F
             for u in nb_in_F:
-                if new_G.number_of_edges(u, n) == 2:
+                if new_G.number_of_edges(u, n) > 1:
                     v = n
                     break
             if v is not None:
