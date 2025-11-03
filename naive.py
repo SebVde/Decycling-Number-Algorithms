b63d1e8e9b0169a72acbfa8481dafb4f5aa1ad95
Seb <s.vandeneede.seb200221@gmail.com>
2025-11-03 14:59:21 +0100

    Naive search goes up to |V|-1

--- naive.py
+++ naive.py
@@ -1,10 +1,11 @@
 import networkx as nx
 from itertools import combinations
 
+
 def naive_fvs(graph):
     all_nodes = set(graph.nodes)
 
-    for i in range(1, len(all_nodes) + 1):
+    for i in range(1, len(all_nodes)):
         subsets = combinations(all_nodes, i)
         for subset in subsets:
             subset = set(subset)
@@ -13,6 +14,7 @@ def naive_fvs(graph):
                 return subset
 
     return None
+
 
 def get_decycling_number(graph):
     return len(naive_fvs(graph))
