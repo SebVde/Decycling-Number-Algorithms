e804f097c6a7d328a3fa47fefb6df19839813982
Seb <s.vandeneede.seb200221@gmail.com>
2025-11-10 16:21:21 +0100

    Modified cleanup function and implemented FVS function

--- 2_approx_fvs.py
+++ 2_approx_fvs.py
@@ -2,28 +2,66 @@ import networkx as nx
 
 
 def cleanup(G):
-    G = G.copy()
-    for node in list(G.nodes):
-        if G.degree(node) <= 1:
-            G.remove_node(node)
-    return G
+    new_G = G.copy()
+    while True:
+        to_remove = [node for node in new_G.nodes if new_G.degree(node) <= 1]
+        if len(to_remove) == 0:
+            break
+        new_G.remove_nodes_from(to_remove)
 
-def get_fvs(G):
+    return new_G
+
+
+def find_semidisjoint_cycle(G):
+    for cycle in nx.cycle_basis(G):
+        cnt = 0
+        for node in cycle:
+            if G.degree(node) != 2:
+                cnt += 1
+            if cnt > 1:
+                break
+        if cnt <= 1:
+            return cycle
+    return None
+
+
+def get_fvs(og_G):
     F = set()
-
-    for node in G.nodes:
-        if G.nodes[node].get("weight", 1) == 0:
-            F.add(node)
-
-    G.remove_nodes_from(F)
-
-    G = cleanup(G)
     i = 0
+    stack = []
+    G = og_G.copy()
 
-    while G.order() > 0:
+    while len(G.nodes) > 0:
         i += 1
+        sd_cycle = find_semidisjoint_cycle(G)
+        if sd_cycle is not None:
+            for node in sd_cycle:
+                G.node[node]["weight"] = 0.0
 
-        # a cycle C is semidisjoint if, for every vertex u of C, d(u) = 2 with at most one exception.
-        # if G contains a semidisjoint cycle C then we can remove all vertices of C from G
+        else:
+            gamma = min(1 / (G.degree(node) - 1) for node in G.nodes)
+            for node in G.nodes:
+                G.nodes[node]["weight"] -= gamma * (G.degree(node) - 1)
 
-        # C = nx.cycle_basis(G)
+        to_remove = [node for node in G.nodes if G.nodes[node]["weight"] == 0.0]
+        F.update(to_remove)
+        G.remove_nodes_from(to_remove)
+        stack.extend(to_remove)
+        G = cleanup(G)
+
+    while len(stack) > 0:
+        node = stack.pop()
+        sg = nx.subgraph(og_G, og_G.nodes - (F - {node}))
+        if nx.is_forest(sg):
+            F.remove(node)
+
+    return F
+
+
+def get_decycling_number_2_approx(G):
+    clean_G = cleanup(G)
+    for node in clean_G.nodes:
+        clean_G.nodes[node]["weight"] = 1.0
+
+    fvs = get_fvs(clean_G)
+    return len(fvs)
